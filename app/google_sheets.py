ead42f5c1b7a85bfb024bd0c39df9275e676c319
ArtVik-75 <alypovartem@yandex.ru>
2026-06-05 10:27:57 +0300

    Fix Google credentials loading

--- app/google_sheets.py
+++ app/google_sheets.py
@@ -11,7 +11,10 @@ SCOPES = [
 
 credentials_dict = json.loads(os.getenv("CREDENTIALS_JSON"))
 
-creds = Credentials.from_service_account_info("credentials.json", scopes=SCOPES)
+creds = Credentials.from_service_account_info(
+    credentials_dict,
+    scopes=SCOPES
+)
 
 client = gspread.authorize(creds)
 
